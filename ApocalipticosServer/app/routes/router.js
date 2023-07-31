42c13551356fd3fb7400bbc55669f1e9efd2fbd0
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2023-07-31 16:51:12 -0300

    refactor: renomeamento da rota

--- ApocalipticosServer/app/routes/router.js
+++ ApocalipticosServer/app/routes/router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const apocalypseController = require('../controllers/apocalypseController');
+//const apocalypseController = require('../controllers/apocalypseController');
 
-router.get('/', (req, res) => res.status(200).send('Sistema de Gerenciamento de Recursos Pós-Apocalípticos'));
+router.get('/info', (req, res) => res.status(200).send('Sistema de Gerenciamento de Recursos Pós-Apocalípticos'));
 
 
 module.exports = router;
