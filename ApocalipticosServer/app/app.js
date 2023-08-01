7ef331198562e9c82b1876c23b9e7263133f3b94
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2023-08-01 14:33:36 -0300

    feat: modificação de rota para pessoas
    Recuperar informações caso sejam civis ou guardiões

--- ApocalipticosServer/app/app.js
+++ ApocalipticosServer/app/app.js
@@ -1,5 +1,4 @@
 const app = require('./server');
 
-// const PORT = process.env.SERVER_PORT || 2510;
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => console.log('Server running'));
--- ApocalipticosServer/app/models/apocalypseModel.js
+++ ApocalipticosServer/app/models/apocalypseModel.js
@@ -24,35 +24,59 @@ const getPessoasByFilter = async (body) => {
     const {nome} = body;
     const {datanascimento} = body;
     const {comunidade} = body;
+    const {profissao} = body;
+    const {raaf} = body;
 
     let and = 0;
     
-    let query = `SELECT * FROM pessoas `;
+    let query = `SELECT p.registrounico, p.nome, p.datanascimento, p.comunidade, c.profissao, g.raaf,
+                        (CASE
+                            WHEN g.raaf IS NULL THEN 'Civil'
+                            ELSE 'Guardião' 
+                        END) AS tipopessoa
+                 FROM pessoas p 
+
+                 LEFT JOIN civil c 
+                 ON p.registrounico = c.registrounico 
+
+                 LEFT JOIN guardioes g 
+                 ON p.registrounico = g.registrounico `;
 
     if(registrounico || nome || datanascimento || comunidade){
         query += `WHERE `;
     }
 
     if(registrounico){
-        query += `pessoas.registrounico = ${registrounico} `;
+        query += `p.registrounico = ${registrounico} `;
         and = 1;
     }
 
     if(nome){
         query += (and) ? (`AND `) : (``);
-        query += `pessoas.nome = '${nome}' `;
+        query += `p.nome = '${nome}' `;
         and = 1;
     }
 
     if(datanascimento){
         query += (and) ? (`AND `) : (``);
-        query += `pessoas.datanascimento = '${datanascimento}' `;
+        query += `p.datanascimento = '${datanascimento}' `;
+        and = 1;
+    }
+    
+    if(comunidade){
+        query += (and) ? (`AND `) : (``);
+        query += `p.comunidade = '${comunidade}' `;
         and = 1;
     }
 
-    if(comunidade){
+    if(profissao){
         query += (and) ? (`AND `) : (``);
-        query += `pessoas.comunidade = '${comunidade}' `;
+        query += `c.profissao = '${profissao}' `;
+    }
+
+    if(raaf){
+        query += (and) ? (`AND `) : (``);
+        query += `g.raaf = '${raaf}' `;
     }
 
     console.log(query);
